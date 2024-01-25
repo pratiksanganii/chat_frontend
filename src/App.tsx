aa246781c445527320e506590939c9644a9312f6
Pratik Sangani <pratik@sangani.com>
2024-01-25 16:30:49 +0530

    tailwind config added

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,19 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Dashboard from "./Dashboard";
-import SignUp from "./components/SignUp";
-import Login from "./components/Login";
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Dashboard from './Dashboard';
+import SignUp from './components/SignUp';
+import Login from './components/Login';
 
 function App() {
   return (
-    <div className="App">
+    <div
+      className='App bg-gray-700 text-cyan-50'
+      style={{ height: '100vh', width: '100vw' }}
+    >
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Dashboard />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/sign-up" element={<SignUp />} />
+          <Route path='/' element={<Dashboard />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/sign-up' element={<SignUp />} />
         </Routes>
       </BrowserRouter>
     </div>
